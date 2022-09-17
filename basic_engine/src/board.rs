fa80662955c1b9eacc5c5414f4c851e8026bb7a9
awright <andrew.wright502@gmail.com>
2022-09-17 16:01:10 +1000

    style: Add pre-commit config and associated initial fixes
    Add pre-commit configuration for basic rust linting. Also includes check
    for commit messages that follow conventional commits specificaiton.

--- basic_engine/src/board.rs
+++ basic_engine/src/board.rs
@@ -1227,7 +1227,10 @@ mod make_move {
         castling_reversible,
         "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
     );
-    test_fen_reversible!(position_3_reversible, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
+    test_fen_reversible!(
+        position_3_reversible,
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"
+    );
 
     macro_rules! test_fen_captures {
         ($func:ident, $f:expr) => {
