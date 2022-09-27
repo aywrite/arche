915531c1a9513d3e63fdfd523968afcdb487f5ec
awright <andrew.wright502@gmail.com>
2022-09-27 17:57:21 +1000

    test(zorbrist): Add test for hash key random uniqueness

--- basic_engine/src/zorbrist.rs
+++ basic_engine/src/zorbrist.rs
@@ -13,7 +13,7 @@ pub struct Zorbrist {
 
 impl Zorbrist {
     pub fn new() -> Self {
-        let mut rng: SmallRng = <SmallRng as SeedableRng>::seed_from_u64(0xDEAD_BEEF);
+        let mut rng: SmallRng = <SmallRng as SeedableRng>::seed_from_u64(0x38655440d1b63d78);
         let mut pieces = [[0u64; 64]; 12];
         for b in &mut pieces {
             let mut array = [0u64; 64];
@@ -38,5 +38,22 @@ impl Zorbrist {
 
     pub fn en_passant_key(&self, index: u8) -> u64 {
         self.en_passant[(index % 8) as usize]
+    }
+}
+
+#[cfg(test)]
+mod test_zorbrist {
+    use super::Zorbrist;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_all_random_numbers_unique() {
+        let z = Zorbrist::new();
+        let mut all = z.pieces.iter().flatten().map(|&c| c).collect::<Vec<u64>>();
+        all.push(z.side);
+        all.extend(z.en_passant);
+        let mut unique = all.clone();
+        unique.dedup();
+        assert_eq!(all.len(), unique.len());
     }
 }
